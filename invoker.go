e3b26f5d5454142f1ff4900010db9e154c8c45eb
Harsh K <hkundra@gmail.com>
2018-03-17 18:32:07 +0530

    fixes + store reflectMethod

--- invoker.go
+++ invoker.go
@@ -9,6 +9,7 @@ type invoker struct {
 	methodName string
 
 	// keep it ready for dynamic invoking
+	methodType   reflect.Method
 	methodHandle reflect.Value
 	inpSymbol    []string
 	inpType      []reflect.Type
@@ -21,7 +22,7 @@ func newInvoker(obj interface{}, method string) invoker {
 	ctype := reflect.TypeOf(obj)
 	cvalue := reflect.ValueOf(obj)
 
-	out := invoker{
+	invk := invoker{
 		object:     obj,
 		methodName: method,
 	}
@@ -30,45 +31,45 @@ func newInvoker(obj interface{}, method string) invoker {
 	if ctype.Kind() == reflect.Ptr {
 		// if the underlying method is defined
 		// on address: func (s *struct) MethodName()
-		if _, found = ctype.MethodByName(method); found {
-			out.methodHandle = cvalue.MethodByName(method)
+		if invk.methodType, found = ctype.MethodByName(method); found {
+			invk.methodHandle = cvalue.MethodByName(method)
 		}
 	} else {
 		// since the method was not found, it may have been defined
 		// on the struct directly: func (s struct) MethodName()
-		if _, found = ctype.MethodByName(method); found {
-			out.methodHandle = cvalue.MethodByName(method)
+		if invk.methodType, found = ctype.MethodByName(method); found {
+			invk.methodHandle = cvalue.MethodByName(method)
 		}
 	}
 
-	// if still not found, then there an issue
+	// if still not found, then there is an issue
 	if !found {
 		panic("method not found: " + method + " in " + ctype.Name())
 	}
 
-	out.inpSymbol, out.inpType = func() ([]string, []reflect.Type) {
-		count := out.methodHandle.Type().NumIn() // skip first parameter (me)
+	invk.inpSymbol, invk.inpType = func() ([]string, []reflect.Type) {
+		count := invk.methodType.Type.NumIn() - 1 // skip first parameter (me)
 		inpS := make([]string, count)
 		inpT := make([]reflect.Type, count)
-		for i := 1; i < count; i++ {
-			inpT[i-1] = out.methodHandle.Type().In(i)
+		for i := 1; i < count+1; i++ {
+			inpT[i-1] = invk.methodType.Type.In(i)
 			inpS[i-1] = typeSymbol(inpT[i-1])
 		}
 		return inpS, inpT
 	}()
 
-	out.outSymbol, out.outType = func() ([]string, []reflect.Type) {
-		count := out.methodHandle.Type().NumOut()
+	invk.outSymbol, invk.outType = func() ([]string, []reflect.Type) {
+		count := invk.methodType.Type.NumOut()
 		outS := make([]string, count)
 		outT := make([]reflect.Type, count)
 		for i := 0; i < count; i++ {
-			outT[i] = out.methodHandle.Type().Out(i)
+			outT[i] = invk.methodHandle.Type().Out(i)
 			outS[i] = typeSymbol(outT[i])
 		}
 		return outS, outT
 	}()
 
-	return out
+	return invk
 }
 
 func (i *invoker) invoke(params ...interface{}) []reflect.Value {
