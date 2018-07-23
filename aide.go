eaaa3d12c7d4b0034f144dd6ad41c189fc8aa6fe
Harsh K <hkundra@gmail.com>
2018-07-24 00:14:20 +0530

    aide changes

--- aide.go
+++ aide.go
@@ -1,11 +1,34 @@
 package fuel
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 type Aide struct {
 	Request  *http.Request
 	Response http.ResponseWriter
 
 	// variables extracted from http request
+	query map[string]string
+}
 
+func (a *Aide) Query() map[string]string {
+
+	// If not null => extraction has been done.
+	// So simply return it
+	if a.query != nil {
+		return a.query
+	}
+
+	// For GET requests, parse Request.Form
+	if a.Request.Method == http.MethodGet {
+		a.query = make(map[string]string)
+		for k := range a.Request.Form {
+			a.query[k] = strings.Join(a.Request.Form[k], ",")
+		}
+		return a.query
+	}
+
+	return nil
 }
